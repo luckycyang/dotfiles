98741bb1f1ad4af88ed94ddc1bb47fa09148935d
luckycyang <166975192+luckycyang@users.noreply.github.com>
2025-02-21 12:43:09 +0800

    Update cmp.lua

--- .config/nvim/lua/config/completion/cmp.lua
+++ .config/nvim/lua/config/completion/cmp.lua
@@ -1,42 +1,77 @@
 require("luasnip").config.setup({ enable_autosnippets = true, store_selection_keys = "<Tab>" })
 require("luasnip.loaders.from_vscode").lazy_load()
+-- Set up nvim-cmp.
+local cmp = require 'cmp'
 
-local cmp = require("cmp")
 cmp.setup({
-	autoEnableSources = true,
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	experimental = { ghost_text = true },
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = require("lspkind").cmp_format({
-			ellipsis_char = "...",
-			maxwidth = 50,
-			symbol_map = { Copilot = "" },
-		}),
-	},
-	mapping = {
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-	},
-	performance = { debounce = 60, fetchingTimeout = 200, maxViewEntries = 30 },
-	sources = {
-		{ name = "nvim_lsp" },
-		{ keywordLength = 3, name = "buffer", option = { get_bufnrs = vim.api.nvim_list_bufs } },
-		{ name = "cmdline" },
-		{ name = "path" },
-		{ keywordLength = 3, name = "luasnip" },
-	},
-	window = { completion = { border = "solid" }, documentation = { border = "solid" } },
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+      -- For `mini.snippets` users:
+      -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+      -- insert({ body = args.body }) -- Insert at cursor
+      -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+      -- require("cmp.config").set_onetime({ sources = {} })
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = {
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+require("cmp_git").setup() ]] --
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
 })
